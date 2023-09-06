662fe9b8603158ddbb0024808a9a5d795b30176b
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-09-06 15:57:24 +0300

    s

--- Ticari Otomasyon/Models/Siniflar/Urun.cs
+++ Ticari Otomasyon/Models/Siniflar/Urun.cs
@@ -12,7 +12,7 @@ namespace Ticari_Otomasyon.Models.Siniflar
         public int Urunid { get; set; }
         public string UrunAd { get; set; }
         public string Marka { get; set; }
-        public short Stok { get; set; }
+        public short stok { get; set; }
         public decimal AlisFiyat { get; set; }
         public decimal SatisFiyat { get; set; }
         public bool Durum { get; set; }

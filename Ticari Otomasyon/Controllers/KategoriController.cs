ecdcb36d6be8819da350b16f5fc3d5c922a40c98
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-09-11 18:09:49 +0300

    İlgili sayfaya kategori isimleri databaseden getirildi. Kategoriler sayfasına sil ve güncelle butonları eklendi.

--- Ticari Otomasyon/Controllers/KategoriController.cs
+++ Ticari Otomasyon/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ticari_Otomasyon.Models.Siniflar;
 
 namespace Ticari_Otomasyon.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
         }
     }
 }
